5f8be0405849d252c9232aa6f85939f3b4c09f4e
Tom Stejskal <tomstejskal@Toming-iMac.local>
2020-12-06 10:42:38 +0100

    Added Breakable button

--- js/script.js
+++ js/script.js
@@ -8,4 +8,15 @@ function changeColor(color, id){
 //Show and hide
 function hideAndSeek() {
     document.getElementById("hideandseek").classList.toggle("hidden");
-  }
+}
+var no = 0;
+//break button
+function breakButton() {
+    no++;
+    if (no == 5){
+        document.getElementById("breakbutt").disabled = true;
+        document.getElementById("breakbutt").innerHTML = "Broken"
+    }
+    
+    document.getElementById("breakText").innerHTML = "If you 10x press this button, the button will break (" + no + ")";
+}
