99b074a375d6e70748a62ac7f0a3a906297360d7
Tom Stejskal <tomstejskal@Toming-iMac.local>
2020-12-06 11:33:28 +0100

    Typo correction

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@
 //Color changer
 function changeColor(color, id){
 	document.getElementById(id).style.color = color;
-	
 }
 
 //Show and hide
 function hideAndSeek() {
     document.getElementById("hideandseek").classList.toggle("hidden");
 }
+
 var no = 0;
 //break button
 function breakButton() {
