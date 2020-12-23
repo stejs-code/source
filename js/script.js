a1650958fa0020ba669d42bba81608a013b30005
stejs <tom@balon.cz>
2020-12-23 09:10:19 +0100

    Added Memory button

--- js/script.js
+++ js/script.js
@@ -20,3 +20,41 @@ function breakButton() {
     
     document.getElementById("breakText").innerHTML = "If you 5x press this button, the button will break (" + no + ")";
 }
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+
+
+function buttonChange(id){
+    var value = getCookie("button-" + id)
+    if (value == "true") {
+        document.cookie = "button-" + id + "=false";
+        console.log("changed to false – " + "button-" + id)
+    } else {
+        document.cookie = "button-" + id + "=true";
+        console.log("changed to true")
+    }
+}
+function buttonCheck(id){
+    console.log("divnej pokus")
+    var value = getCookie("button-" + id)
+    if (value == "true"){
+        document.getElementById(id).checked = true;;
+        console.log("loadnuto voe")
+    }
+}
+window.onload = buttonCheck('switcher');
